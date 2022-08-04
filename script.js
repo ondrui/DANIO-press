39521838fc364c5257b1b657638f0e1ae2f73abe
ondrui <ondrui@yandex.ru>
2022-08-04 12:42:38 +0300

    version 1.0.0

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 let traffic = document.querySelector('#trafficlight');
 
-let colorsTraffic = {
-  red: 'red',
-  green: 'green',
-  yellow: 'yellow',
-  redYellow: 'red yellow',
-};
-
 let socket = io();
 
 socket.on('connect', () => {
   socket.on('message', (data) => {
-    console.log(data);
-    if ((data === 'get color')) {
-      console.log(traffic.className);
+    if (data === 'get color') {
       socket.emit('message', traffic.className);
     } else {
-      console.log(traffic.className);
-      console.log(data);
       traffic.className = data;
-      console.log(traffic.className);
     }
   });
 });
--- server.js
+++ server.js
@@ -18,29 +18,21 @@ const randomNum = (min = 2000, max = 10000) => {
 };
 
 const colorBlink = (socket) => {
-  console.log('colorBlink start');
   socket.emit('message', 'get color');
   socket.on('message', (message) => {
-    console.log(message);
     switch (message) {
       case 'red':
-        console.log('red yellow');
         return socket.emit('message', 'red yellow');
       case 'red yellow':
-        console.log('green');
         return socket.emit('message', 'green');
       case 'green':
-        console.log('yellow');
         return socket.emit('message', 'yellow');
       case 'yellow':
-        console.log('red');
         return socket.emit('message', 'red');
       case '':
-        console.log('yellow');
         return socket.emit('message', 'yellow');
       default:
         socket.on('disconnect', function () {
-          console.log('A user disconnected');
           return socket.disconnect(true);
         });
         return console.log('wrong color');
